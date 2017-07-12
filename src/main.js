049e2fe267f4b2e7ca5fbd00f5bdf9a3f3e6d65d
shenfangda520 <964003502@qq.com>
2017-07-12 09:56:20 +0800

    VUE创建项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+//
+import Header from './components/Header'
+Vue.component('v-header', Header)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+//大页面模块
+import Index from '@/views/one'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'index',
+      component: Index
     }
   ]
 })
